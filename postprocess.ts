6d038470bba6d9e755381de6fd5b684d108da5b8
Roman Gusev <Roman_Gusev1@epam.com>
2021-05-22 12:17:47 +0600

    Add postprocess script

--- postprocess.ts
+++ postprocess.ts
@@ -1,1 +1,19 @@
-console.log("ping");
+import {
+  readJSON,
+  readCSV,
+  writeCSV,
+} from "https://deno.land/x/flat@0.0.10/mod.ts";
+
+const filename = Deno.args[0];
+const json = await readJSON(filename);
+
+const newFilename = "history.csv";
+const history = await readCSV(newFilename);
+
+history.push({
+  temperature: json.temperature,
+  description: json.description,
+  date: new Date().toISOString(),
+});
+
+await writeCSV(newFilename, history);
