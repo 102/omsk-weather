60b10324bcb9ba53ed568d771a2065443107b717
Roman Kenig-Gusev <rgusev2@gmail.com>
2025-10-30 12:17:57 +0100

    Append new data instead of rewriting

--- postprocess.ts
+++ postprocess.ts
@@ -1,17 +1,16 @@
-import {
-  readCSV,
-  readJSON,
-  writeCSV,
-} from "https://deno.land/x/flat@0.0.15/mod.ts";
+import { readJSON, writeTXT } from "https://deno.land/x/flat@0.0.15/mod.ts";
 
 const filename = Deno.args[0];
 const json = await readJSON(filename);
 
 const newFilename = "history.csv";
-const history = await readCSV(newFilename);
 
-await writeCSV(newFilename, [{
-  "temperature in °C": json.main.temp,
-  description: json.weather[0].main,
-  date: new Date().toISOString(),
-}, ...history]);
+const wrapDescription = (description: string) => (description.includes(",") ? `"${description}"` : description);
+
+await writeTXT(
+  newFilename,
+  [json.main.temp, wrapDescription(json.weather[0].main), new Date().toISOString()].join(",") + "\n",
+  {
+    append: true,
+  }
+);
